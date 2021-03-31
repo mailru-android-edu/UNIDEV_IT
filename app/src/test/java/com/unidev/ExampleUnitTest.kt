6ae04d911877d5ff3b41ab8202769ddbb65d70d6
Danil_Gronin <danila.gronin@gmail.com>
2021-03-31 23:23:37 +0300

    The first scilet version of app was created

--- app/src/test/java/com/unidev/ExampleUnitTest.kt
+++ app/src/test/java/com/unidev/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
-package com.unidev.unidevfitness
+package com.unidev
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
