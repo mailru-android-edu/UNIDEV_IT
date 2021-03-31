0bc43fce10d0185d99b46e86d933b1bc73f3898d
Roman Shadko <61965857+ro-SH@users.noreply.github.com>
2021-03-31 17:01:32 +0300

    Improved codestyle

--- UnidevFitnessApp/app/src/main/java/com/unidev/unidevfitness/FirstFragment.kt
+++ UnidevFitnessApp/app/src/main/java/com/unidev/unidevfitness/FirstFragment.kt
@@ -6,18 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FirstFragment: Fragment() {
+class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
 }
--- UnidevFitnessApp/app/src/main/java/com/unidev/unidevfitness/MainActivity.kt
+++ UnidevFitnessApp/app/src/main/java/com/unidev/unidevfitness/MainActivity.kt
@@ -3,7 +3,7 @@ package com.unidev.unidevfitness
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
