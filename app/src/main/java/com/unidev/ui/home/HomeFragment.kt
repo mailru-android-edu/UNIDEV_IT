9a57c2e6b3b97bc75bd1de5052014a586592e7d9
Roman Shadko <61965857+ro-SH@users.noreply.github.com>
2021-05-12 16:55:09 +0300

    Add first version of home interface

--- app/src/main/java/com/unidev/ui/home/HomeFragment.kt
+++ app/src/main/java/com/unidev/ui/home/HomeFragment.kt
@@ -4,31 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.unidev.R
+import com.unidev.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var homeViewModel: HomeViewModel
+
+    private lateinit var adapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(
-            viewLifecycleOwner,
-            Observer {
-                textView.text = it
-            }
+    ): View {
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home, container, false
         )
-        return root
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        binding.homeViewModel = homeViewModel
+        binding.lifecycleOwner = this
+
+        binding.fragmentHomeIbEdit.setOnClickListener {
+            Toast.makeText(context, "Edit pressed!", Toast.LENGTH_SHORT).show()
+        }
+
+        // News
+        adapter = NewsAdapter(homeViewModel.news)
+        binding.fragmentHomeRvNews.adapter = adapter
+        binding.fragmentHomeRvNews.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val pagerSnapHelper = PagerSnapHelper()
+        pagerSnapHelper.attachToRecyclerView(binding.fragmentHomeRvNews)
     }
 }
--- app/src/main/java/com/unidev/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/unidev/ui/home/HomeViewModel.kt
@@ -3,11 +3,72 @@ package com.unidev.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.unidev.R
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _age = MutableLiveData<Int>()
+    val age: LiveData<Int>
+        get() = _age
+
+    private val _city = MutableLiveData<String>()
+    val city: LiveData<String>
+        get() = _city
+
+    private val _country = MutableLiveData<String>()
+    val country: LiveData<String>
+        get() = _country
+
+    private val _guide = MutableLiveData<String>()
+    val guide: LiveData<String>
+        get() = _guide
+
+    private val _height = MutableLiveData<Int>()
+    val height: LiveData<Int>
+        get() = _height
+
+    private val _index = MutableLiveData<Double>()
+    val index: LiveData<Double>
+        get() = _index
+
+    private val _name = MutableLiveData<String>()
+    val name: LiveData<String>
+        get() = _name
+
+    private val _news: MutableList<NewsItem> = generateList().toMutableList()
+    val news: List<NewsItem>
+        get() = _news
+
+    private val _subscription = MutableLiveData<String>()
+    val subscription: LiveData<String>
+        get() = _subscription
+
+    private val _weight = MutableLiveData<Int>()
+    val weight: LiveData<Int>
+        get() = _weight
+
+    init {
+        _age.value = 20
+        _city.value = "Москва"
+        _country.value = "Россия"
+        _guide.value = "Похудение"
+        _height.value = 177
+        _index.value = 0.0
+        _name.value = "Иван Иванов"
+        _subscription.value = "Silver"
+        _weight.value = 70
     }
-    val text: LiveData<String> = _text
+
+    fun setName(firstName: String, secondName: String) {
+        _name.value = "$firstName $secondName"
+    }
+}
+
+fun generateList(): List<NewsItem> {
+    return listOf(
+        NewsItem("Какой-то заголовок", "27 Янв 2020 19:10", "Какой-то текст", R.mipmap.ic_launcher),
+        NewsItem("Какой-то заголовок", "27 Янв 2020 19:10", "Какой-то текст", R.mipmap.ic_launcher),
+        NewsItem("Какой-то заголовок", "27 Янв 2020 19:10", "Какой-то текст", R.mipmap.ic_launcher),
+        NewsItem("Какой-то заголовок", "27 Янв 2020 19:10", "Какой-то текст", R.mipmap.ic_launcher)
+    )
 }
